1c882ede517f4f7de694f906f14da2bafdfdd498
Malcolm Morgan <mem48@users.noreply.github.com>
2023-12-10 10:26:11 +0000

    Add registration

---
+++ js/register.js
@@ -0,0 +1,73 @@
+function registerDevice(){
+
+  
+  var email = document.getElementById("email").value;
+  var email2 = document.getElementById("email2").value;
+  
+  if(email != email2){
+    alert("Emails do not match");
+    return null
+  }
+  
+  if(!validateEmail(email)){
+    alert("Invalid Email Address");
+    return null
+  }
+  
+  
+  var did = getCookie("BCC_id");
+  
+  var register_url = "https://script.google.com/macros/s/AKfycbzTKENIGp3Roy4QWw6WLwmGpKKol-8ymCLqPsEspI6hg8dVmUeOd8cs_lEYo8F6uhvRBw/exec"
+
+  var query_url = register_url +
+      "?email=" + email +
+      "&did=" + did;
+      
+  console.log(query_url);
+      
+  fetch(query_url, {
+      redirect: "follow",
+      method: "GET",
+      headers: {
+        "Content-Type": "text/plain;charset=utf-8"
+      }
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json(); // Parse the JSON response
+  })
+  .then(data => {
+    if(data.result == "success"){
+      setCookie("registered","true");
+      closeregistration();
+    } else {
+      console.log("Failed to submit ");
+      console.log(data);
+    }
+  })
+  
+  .catch(error => {
+    console.error('Error:', error); // Log any errors
+  });
+
+}
+
+function validateEmail(email) {
+  var re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return re.test(email);
+}
+
+function closeregistration(){
+  console.log("Checking form")
+  var reg = getCookie("registered");
+  
+  if(reg === "true"){
+    var form = document.getElementById("registationform");
+    form.innerHTML = "<p><b>Your device is registered<b></p>"
+  }
+  
+}
+
+closeregistration();
